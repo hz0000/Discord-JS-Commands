b6b4b3643f2c6608aa72f2084b34a9b5f69907b9
HZ <88983269+hz0000@users.noreply.github.com>
2021-10-14 15:56:50 +0000

    Add files via upload

---
+++ Commands/V12/banner.js
@@ -0,0 +1,32 @@
+const Discord = require("discord.js");
+module.exports = {
+        config: {
+            name: "banner",
+            aliases: ['banner'],
+            description: "pegar o banner",
+            example: "banner <@user/ID>",
+            usage: 'banner @user'
+        },
+        run: async (bot, message, args) => {
+            message.delete()
+            const user = message.mentions.users.first() || message.guild.members.cache.get(args[0]) || message.author
+            let B = await bot.api.users(`${user.id}`).get(" ")
+
+            let msg = new Discord.MessageEmbed() 
+            .setColor('#ff58c3') 
+.setTitle(`Banner solicitado`)
+            .setImage(`https://cdn.discordapp.com/banners/${user.id}/${B.banner}.png?size=512`) 
+            .setFooter(`Comando requisitado por: ${message.author.tag}`, message.author.displayAvatarURL({format: "png"}));
+
+            let gif = new Discord.MessageEmbed() 
+            .setColor("#ff58c3")
+.setTitle(`Banner solicitado`) 
+            .setImage(`https://cdn.discordapp.com/banners/${user.id}/${B.banner}.gif?size=512`) 
+            .setFooter(`${message.author.tag}`, message.author.displayAvatarURL({format: "png"}));
+
+            if (B.banner.startsWith("a_")){
+                message.channel.send(gif)
+            } else{
+                message.channel.send(msg)
+            }
+        }}
